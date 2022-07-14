7a8192cac9bac7521b218724662d0fc1b41c24e6
PC <PC@DESKTOP-R8SP3AG>
2022-07-14 11:16:48 -0700

    Third Commit

---
+++ Day2_Intro_to_Java/src/com/cg/intro2/Program2.java
@@ -0,0 +1,25 @@
+package com.cg.intro2;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int arr[]=new int[n];
+		//to enter the array elements
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+		}
+		//to print the array elements
+		for(int i:arr)
+		{
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
