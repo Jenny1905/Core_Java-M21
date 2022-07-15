df53b0403e31ff7265daa4414256ae0d7265191a
PC <PC@DESKTOP-R8SP3AG>
2022-07-15 12:49:36 -0700

    First Commit

---
+++ Day4_Encapsulation,Packages,Abstraction/src/com/cg/encap/pack3/HDFC.java
@@ -0,0 +1,19 @@
+package com.cg.encap.pack3;
+
+public class HDFC 
+{
+	private int pin=1221;
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	
+
+	
+
+}
