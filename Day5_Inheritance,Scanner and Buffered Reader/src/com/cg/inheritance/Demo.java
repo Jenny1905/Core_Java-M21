eb5c72835a0591e0a3a3bd6a8920a58eb9ff7d6a
PC <PC@DESKTOP-R8SP3AG>
2022-07-16 19:22:14 -0700

    First Commit

---
+++ Day5_Inheritance,Scanner and Buffered Reader/src/com/cg/inheritance/Demo.java
@@ -0,0 +1,34 @@
+package com.cg.inheritance;
+class Mother
+{
+	static int val=5;
+	static int display()
+	{
+		return val;
+		//System.out.println("My Mother name is: Vandana");
+		
+	}
+}
+class Daughter extends Mother
+{
+	//int val=5;
+	static int display()
+	{
+		return val;
+		//System.out.println("My Father name is: Suresh");
+	}
+}
+public class Demo 
+{
+
+	public static void main(String[] args)
+	{
+		
+		Daughter d=new Daughter();
+		System.out.println(d.display());
+		
+		
+
+	}
+
+}
