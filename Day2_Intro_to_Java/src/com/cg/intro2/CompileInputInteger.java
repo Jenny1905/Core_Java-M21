d2913ea51ba44f959a316e8bd7c16317023b9404
PC <PC@DESKTOP-R8SP3AG>
2022-07-20 10:32:52 -0700

    Third Commit

--- Day2_Intro_to_Java/src/com/cg/intro2/CompileInputInteger.java
+++ Day2_Intro_to_Java/src/com/cg/intro2/CompileInputInteger.java
@@ -2,9 +2,10 @@ package com.cg.intro2;
 
 public class CompileInputInteger {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		int a=25;
-		System.out.println(a);
+		//System.out.println(a);
+		return a;
 
 	}
 
